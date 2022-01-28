35ec5c06c4c5440aa7c05da39f792db588f18fce
Averiser <averiser@gmail.com>
2022-01-28 19:49:28 +0300

    first commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by MyMacBook on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+  var ourTimer = Timer()
+  var timerDisplayed = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
