9a3d3945ce1af1b324b1cd1567b7e59b9841352d
Averiser <averiser@gmail.com>
2022-01-29 19:29:58 +0300

    second commit - adding starting a timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  
   var ourTimer = Timer()
   var timerDisplayed = 0
+  
+  @IBOutlet weak var label: UILabel!
+  
+  @IBAction func startBTN(_ sender: Any) {
+    ourTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+  }
+  
+  @IBAction func pauseBTN(_ sender: Any) {
+  }
+  
+  @IBAction func resetBTN(_ sender: Any) {
+  }
+  
+  @objc func Action() {
+    timerDisplayed += 1
+    label.text = String(timerDisplayed)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
